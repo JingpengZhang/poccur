b46c18ff9ef81dc97b295b7727ccbde43bc64586
JingpengZhang <jingpeng_zhang@foxmail.com>
2023-08-17 22:20:53 +0800

    feat:1.配置icon插件；2.配置tailwindcss；3.配置字体；

--- app.vue
+++ app.vue
@@ -1,5 +1,11 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <Icon name="uil:github" color="black" />
+    <p class="test font-noto font-thin">Hello Nuxt 张晶鹏</p>
+    <p class="test font-noto font-light">Hello Nuxt 张晶鹏</p>
+    <p class="test font-noto">Hello Nuxt 张晶鹏</p>
+    <p class="test font-noto font-medium">Hello Nuxt 张晶鹏</p>
+    <p class="test font-noto font-bold">Hello Nuxt 张晶鹏</p>
+    <p class="test font-noto font-black">Hello Nuxt 张晶鹏</p>
   </div>
 </template>
---
+++ tailwind.config.ts
@@ -0,0 +1,26 @@
+import { Config } from "tailwindcss";
+import defaultTheme from "tailwindcss/defaultTheme";
+
+export default <Partial<Config>>{
+  content: [
+    "./components/**/*.{vue,js,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./app.vue",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        noto: ["NotoSans"],
+      },
+    },
+    fontWeight: {
+      thin: "100",
+      light: "300",
+      regular: "400",
+      medium: "500",
+      bold: "700",
+      black: "900",
+    },
+  },
+};
